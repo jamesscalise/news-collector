e354c050fc30a9e1a738e9e5b024cef0579fc707
James Scalise <jamescscalise@gmail.com>
2019-10-08 23:14:05 +0000

    news changes

--- lib/first_project/news.rb
+++ lib/first_project/news.rb
@@ -1,11 +1,14 @@
 class FirstProject::News
-  @headlines, @url, @text
+ attr_accessor :headline, :url
   def self.today
     self.scrape_headlines
   end
   
   def self.scrape_headlines
     news = []
-    doc = Nokogiri::HTML(open(https://www.statesman.com/))
+    doc = Nokogiri::HTML(open("https://www.statesman.com/"))
+    doc.css(".stories .summary").each do |story|
+      
+    end
   end
 end
