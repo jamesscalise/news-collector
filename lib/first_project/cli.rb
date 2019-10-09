75db8e7d9fd0998f502633cddd4208350fd18050
James Scalise <jamescscalise@gmail.com>
2019-10-09 00:28:58 +0000

    article scraping

--- lib/first_project/cli.rb
+++ lib/first_project/cli.rb
@@ -8,17 +8,24 @@ class FirstProject::CLI
   end
   
   def list_news
-    puts "Here are today's top news stories:"
+    puts "Here are the top news stories right now:"
+    @news = FirstProject::News.now
+    @news.each_with_index do |news, index|
+      puts "#{index+1}. #{news.headline}"
+    end
   end
   
   def menu
+    
     while true
-      puts "Enter the number of the article to read, type list to see the headlines again, or type exit:"
+      puts "Enter the number of the article to read, type update to see the newest headlines, or type exit:"
       input = gets.strip.downcase
       if input == "exit"
         break
-      elsif input.to_i > 0
-      elsif input == "list"
+      elsif input.to_i > 0 && input.to_i < 6
+        text = FirstProject::News.article_scrape(@news[input.to_i].url)
+        
+      elsif input == "update"
         list_news
       else
         puts "Please put a valid input"
--- lib/first_project/news.rb
+++ lib/first_project/news.rb
@@ -13,11 +13,18 @@ class FirstProject::News
     doc = Nokogiri::HTML(open("https://www.statesman.com/"))
     doc.css(".stories .summary").each do |story|
       item = self.new
-      item.headline = story.css("a").text
+      item.headline = story.css("a").text.gsub(/[\t\r\n]/,'')
       item.url = story.css("a").attr("href").value
+      news << item
+    end
+    news
+    
+  end
+  
+  def self.article_scrape(url)
+    page = Nokogiri::HTML(open(url))
+    page.css(".article-body div.inner p").each do |paragraph|
       binding.pry
     end
-    
-    news
   end
 end
